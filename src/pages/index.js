41682b35d59efec9972e7220d09d91ad3ebd8d0b
Sammy <sammysamkough@hotmail.com>
2022-10-11 07:58:00 -0400

    anotha one

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,78 +9,96 @@ class Home extends React.Component {
 
     return (
       <Layout>
-        {developers.map(({ id, title, description, logo, collections }) => (
-          <div
-            key={id}
-            style={{
-              maxWidth: '700px',
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '12px',
-              padding: '8px',
-              border: '2px dotted black',
-              borderRadius: '2px',
-              cursor: 'pointer',
-            }}
-          >
+        <div style={{ width: '100%', display: 'flex', gap: '30px' }}>
+          {developers.map(({ id, title, description, logo, collections }) => (
             <div
+              key={id}
               style={{
+                maxWidth: '700px',
                 display: 'flex',
+                flexDirection: 'column',
                 justifyContent: 'space-between',
-                flexWrap: 'wrap-reverse',
-                gap: '8px',
+                gap: '12px',
+                padding: '8px',
+                border: '2px dotted black',
+                borderRadius: '2px',
+                cursor: 'pointer',
               }}
             >
               <div
                 style={{
-                  maxWidth: '350px',
                   display: 'flex',
-                  flexDirection: 'column',
+                  justifyContent: 'space-between',
+                  flexWrap: 'wrap',
                   gap: '8px',
                 }}
               >
-                <h2 style={{ margin: '0px', padding: '0px' }}>{title}</h2>
-                <span style={{ fontSize: '20px' }}>
-                  {JSON.parse(description.raw).content[0].content[0].value}
-                </span>
-              </div>
-              <img
-                src={logo.url}
-                alt={logo.description}
-                style={{ maxWidth: '300px', height: 'auto' }}
-              />
-            </div>
-            <hr style={{ width: '100%' }} />
-            <span style={{ fontSize: '18px' }}>
-              <em>Featured Collections</em>
-            </span>
-            <div style={{ width: '100%', display: 'flex' }}>
-              {collections.map(
-                ({
-                  id: collectionId,
-                  title: collectionTitle,
-                  logo: collectionLogo,
-                }) => (
-                  <div
-                    key={collectionId}
+                <div
+                  style={{
+                    maxWidth: '350px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '8px',
+                  }}
+                >
+                  <h2 style={{ margin: '0px', padding: '0px' }}>{title}</h2>
+                  <span
                     style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      gap: '4px',
-                      cursor: 'pointer',
+                      display: '-webkit-box',
+                      '-webkit-line-clamp': 3,
+                      '-webkit-box-orient': 'vertical',
+                      overflow: 'hidden',
+                      fontSize: '20px',
                     }}
                   >
-                    <h4 style={{ margin: 0, padding: 0 }}>{collectionTitle}</h4>
-                    <img
-                      src={collectionLogo.url}
-                      style={{ maxWidth: '150px', height: 'auto' }}
-                    />
-                  </div>
-                )
-              )}
+                    {JSON.parse(description.raw).content[0].content[0].value}
+                  </span>
+                </div>
+                <img
+                  src={logo.url}
+                  alt={logo.description}
+                  style={{ maxWidth: '100px', height: '100px' }}
+                />
+              </div>
+              <div
+                style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}
+              >
+                <hr style={{ width: '100%' }} />
+                <span style={{ fontSize: '18px' }}>
+                  <em>Featured Collections</em>
+                </span>
+                <div style={{ width: '100%', display: 'flex' }}>
+                  {collections.map(
+                    ({
+                      id: collectionId,
+                      title: collectionTitle,
+                      logo: collectionLogo,
+                    }) => (
+                      <div
+                        key={collectionId}
+                        style={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          gap: '4px',
+                          cursor: 'pointer',
+                        }}
+                      >
+                        <h4 style={{ margin: 0, padding: 0 }}>
+                          {collectionTitle}
+                        </h4>
+                        <img
+                          src={collectionLogo.url}
+                          alt={collectionLogo.description}
+                          style={{ maxWidth: '150px', height: 'auto' }}
+                        />
+                      </div>
+                    )
+                  )}
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </Layout>
     )
   }
